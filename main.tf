1c46be3691a8a2f9151fe5e828cf9e73c7353a5c
Abdulmalik <lawalabdulmalik59@gmail.com>
2025-01-03 19:57:42 +0100

    fixed the tf plan issue

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}
 
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length = 16
+  special = false
+  lower = true
+}
+
+output "random_string_id" {
+  value = random_string.bucket_name.id
+}
+output "random_string_result" {
+  value = random_string.bucket_name.result
+}
